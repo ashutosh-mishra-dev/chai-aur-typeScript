3ff0b202baf240f3989cfe96d9d51d9eb04fc651
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-12-19 09:48:27 +0530

    narrowing type and gaurd type examples run on terminal

--- src/03_typesNarrowing.ts
+++ src/03_typesNarrowing.ts
@@ -1,31 +1,44 @@
 /*
 Lecture 6: Type Narrowing & Type Guards
+
+Type guard = check(typeof, instanceof, if, comparisons)
+Type narrowing = us check ka result
+
 English : Type narrowing is the process of refining a broader type (like a union) into a more specific type at runtime using checks.
+
+English : Type guards are conditions or functions that help TypeScript determine the specific type of a variable.
+👉 Type guards are tools used to achieve type narrowing.
+
+Hindi : Type guards wo checks hote hain jo TypeScript ko batate hain ki variable ka exact type kya hai.
+👉 Type guards ka use karke hi type narrowing hoti hai.
 */
 
-function getChai(kind: string | number) {
+//--------------------------- Example 1: getChai -> import by index.ts --------------------------------------
+export function getChai(kind: string | number) {
   // agar ham kind pr hover karte ha to : (parameter) kind: string | number matlab yha string ya to number aa sakta hai
   if (typeof kind === "string") {
     return `making ${kind} of chai...`; // but jab ham yha kind pr hover karte h to yha data types ek dam sure ho jata h ex.agar ham kind pr hover karte ha to : (parameter) kind: string | number
   }
-  return `chai order ${kind}`; /// same yha bhi kind pr hover karte h to yha data types ek dam sure ho jata h ex. (parameter) kind: number
+  return `chai orderId : ${kind}`; /// same yha bhi kind pr hover karte h to yha data types ek dam sure ho jata h ex. (parameter) kind: number
 }
 
 //  saf saf dekh sakta hu ki hya hame gauranteed data typemil rha hai agar ham string vale kind pr kind. "kind(dot)" karte hi hame string ka method suggest hota hai
 //agar ham number wale kind pr kind. karte hai to math method suggest karta hai ye most importaint suggestions hote h jo production me kaam aata hai.
 //ab esi ko Narrowing type hota hai.
 
-//found truthyness :
-function serveChai(msg?: string) {
+//found Truthiness narrowing :
+// ---------------------------- Example 2: serveChai -> import by index.ts --------------------------------
+export function serveChai(msg?: string) {
   if (msg) {
     return `serving ${msg}`;
   }
   return `serving default masala chai`;
 }
 
+// ---------------------------- Example 3: orderChai -> import by index.ts --------------------------------
 // axostic checks :
-
-function orderChai(size: "small" | "medium" | "large" | number) {
+//Literal type narrowing:
+export function orderChai(size: "small" | "medium" | "large" | number) {
   if (size === "small") {
     return `small cutting chai...`;
   }
@@ -35,25 +48,31 @@ function orderChai(size: "small" | "medium" | "large" | number) {
   return `chai order #${size}`;
 }
 
-class KulhadChai {
+// ---------------------------- Example 4: using class -> import by index.ts --------------------------------
+//Class-based narrowing :
+export class KulhadChai {
   serve() {
     return `Serving Kulhad Chai`;
   }
 }
 
-class CuttingChai {
+export class CuttingChai {
   serve() {
     return `Serving Cutting Chai`;
   }
 }
 
-function serve(chai: KulhadChai | CuttingChai) {
+export function serve(chai: KulhadChai | CuttingChai) {
   if (chai instanceof KulhadChai) {
     return chai.serve(); // to ab hame yha pata hai ki kulhadChai wale class ka method call hoga.
+  } else {
+    return chai.serve(); // yha baki bacha hua class ka method call hoga means CuttingChai class ka method hi call hoga
   }
 }
 
 //***hame typescript ek super power deta h khud ke types define karne ke using "type" keyword; type ek guard type checking hai.
+
+//----------------------------------------------------------------------------------------------
 
 type chaiOrder = { type: string; sugar: number };
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
 function greet(person: string): string {
   return `Hello ${person}, welcome to Chai Code`;
 }
-
 const username: string = "chai aur typeScript";
+//console.log(greet(username));
 
-console.log(greet(username));
+//======================== here we are using src/03_typesNarrowing.js file here ==============================
+
+import {
+  CuttingChai,
+  getChai,
+  KulhadChai,
+  orderChai,
+  serve,
+  serveChai,
+} from "./03_typesNarrowing.js";
+
+//---------- Example 1: getChai -------------------
+// console.log(getChai("green"));
+// console.log(getChai(3));
+
+//---------- //Example 2: serveChai -------------------
+// console.log(serveChai("elaichi"));
+// console.log(serveChai());
+
+//---------- Example 3: orderChai -------------------
+// console.log(orderChai("small"));
+// console.log(orderChai("large"));
+// console.log(orderChai(3));
+
+//---------- Example 4: using class -------------------
+const Kulhad = new KulhadChai();
+const cutting = new CuttingChai();
+
+// console.log(serve(Kulhad));
+// console.log(serve(cutting));
+
+//============================================================================================================
